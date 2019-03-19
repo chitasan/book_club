a2e89a6d32434e4f146211f0c13fb34172f6ab89
Chi Tran <chidtran@gmail.com>
2019-03-19 16:07:10 -0600

    Adds AuthorBooks migration
    Co-authored-by: Trevor Noland <42710030+tnodland@users.noreply.github.com>

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,10 @@
 class Author < ApplicationRecord
+  has_many :author_books
+  has_many :books, through: :author_books
 
+  validates_presence_of :name
+
+  def self.with_books
+      select("books.*, authors.*").joins(:books)
+  end 
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,10 @@
 class Book < ApplicationRecord
+  has_many :author_books
+  has_many :authors, through: :author_books
+
   validates_presence_of :title, :pages, :pub_date
+
+  def self.with_authors
+    select("books.*, authors.*").joins(:authors)
+  end 
 end
