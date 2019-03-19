dce182a4325b8455039110d034b72d10c1da8cf2
Chi Tran <chidtran@gmail.com>
2019-03-19 16:11:28 -0600

    Deletes extraneous bs
    Co-authored-by: Trevor Noland <42710030+tnodland@users.noreply.github.com>

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,8 +3,4 @@ class Author < ApplicationRecord
   has_many :books, through: :author_books
 
   validates_presence_of :name
-
-  def self.with_books
-      select("books.*, authors.*").joins(:books)
-  end 
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,8 +3,4 @@ class Book < ApplicationRecord
   has_many :authors, through: :author_books
 
   validates_presence_of :title, :pages, :pub_date
-
-  def self.with_authors
-    select("books.*, authors.*").joins(:authors)
-  end 
 end
