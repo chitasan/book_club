08a1b913bb6e39f4b313a61a7e79745ffadb2d86
Trevor Nodland <trevor.nodland@gmail.com>
2019-03-23 09:49:55 -0600

    adds sort username method to review class

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,8 @@
     validates_presence_of :title, :rating, :content, :user_name
 
     belongs_to :book
+
+    def self.sort_username(username)
+      Review.where(user_name: username)
+    end
   end
