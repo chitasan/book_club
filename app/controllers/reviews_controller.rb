434fe0bb3af6565712bd009a4ee5b56cf55eb289
Trevor Nodland <trevor.nodland@gmail.com>
2019-03-23 13:04:45 -0600

    finishes user story 13
    Co-authored-by: Chi Tran 44073660+chitasan@users.noreply.github.com

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,6 +5,19 @@ class ReviewsController < ApplicationController
   end
 
   def new
+    @book = Book.find(params[:book_id])
+    @review = Review.new(book: @book)
+  end
 
+  def create
+    @book = Book.find(params[:book_id])
+    @book.reviews.create(review_params)
+    redirect_to (book_path(@book))
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :rating, :content, :user_name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root "home#welcome"
   resources :books, only: [:index, :show] do
-    resources :reviews, only: [:new]
+    resources :reviews, only: [:new, :index, :create]
   end
+  
   resources :authors, only: [:show]
   get '/:username', to: "reviews#index", as: :user_show
 end
