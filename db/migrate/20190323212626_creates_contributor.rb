06eae60524634a767b53bd5896174388907003a1
Trevor Nodland <trevor.nodland@gmail.com>
2019-03-24 10:16:54 -0600

    adds model tests for contributor table, fixes an s

--- db/migrate/20190323212626_creates_contributor.rb
+++ db/migrate/20190323212626_creates_contributor.rb
@@ -1,6 +1,6 @@
 class CreatesContributor < ActiveRecord::Migration[5.1]
   def change
-    create_table :contributor do |t|
+    create_table :contributors do |t|
       t.string :name
       t.string :github
     end
