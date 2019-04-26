031e2d1a7d5797e67584a1caf19eeafc89cdfae4
Chi Tran <chidtran@gmail.com>
2019-04-26 00:50:18 -0600

    Adds mod 3 routes prework

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
     resources :reviews, only: [:new, :index, :create]
   end
 
-
   delete '/books/:id', to: "books#delete", as: :delete_book
   delete '/authors/:id', to: "authors#delete", as: :delete_author
   delete '/:review_id', to: "reviews#delete", as: :delete_review
@@ -12,4 +11,20 @@ Rails.application.routes.draw do
   get '/:username', to: "reviews#index", as: :user_show
   get '/:username/:sort', to: "reviews#index", as: :user_show_sort
   get '/books/:sort/:order', to: "books#index", as: :books_sort
+
+  #mod3 prework routes
+  # get '/books' to: 'books#index'
+  # get '/books/:id', to: 'books#show'
+  # get '/books/new', to: 'books#new'
+  # post '/books', to: 'books#create'
+
+  # get '/books/:id/reviews', to: 'reviews#index'
+  # get '/books/:id/reviews/new', to: 'reviews#new'
+  # post '/books/:id/reviews/', to: 'reviews#create'
+
+  # get '/authors/:id', to: 'authors#show'
+
+  # resources :books, only: [:destroy]
+  # resources :authors, only: [:destroy]
+  # resources :reviews, only: [:destroy]
 end
