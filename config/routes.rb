11150ae8cfdec8d06cd897f22d4c0519d3c5d2a8
Trevor Nodland <trevor.nodland@gmail.com>
2019-04-27 14:19:29 -0600

    updates routes as per the prework

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
   root "home#welcome"
-  resources :books, only: [:index, :show, :new, :create] do
-    resources :reviews, only: [:new, :index, :create]
-  end
+
+  get '/books', to: "books#index", as: :books
+  get '/book/:id', to: "books#show", as: :book
+  get '/books/new', to: "books#new", as: :new_book
+  post '/books', to: "books#create"
+  get '/books/:book_id/reviews', to: "reviews#index", as: :book_reviews
+  get '/books/:book_id/reviews/new', to: "reviews#new", as: :new_book_review
+  post '/books/:book_id/reviews', to: "reviews#create"
+
 
 
   delete '/books/:id', to: "books#delete", as: :delete_book
   delete '/authors/:id', to: "authors#delete", as: :delete_author
   delete '/:review_id', to: "reviews#delete", as: :delete_review
-  resources :authors, only: [:show]
+  get '/authors/:id', to: "authors#show", as: :author
   get '/:username', to: "reviews#index", as: :user_show
   get '/:username/:sort', to: "reviews#index", as: :user_show_sort
   get '/books/:sort/:order', to: "books#index", as: :books_sort
